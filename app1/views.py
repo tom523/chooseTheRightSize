fb05c76aef55126b68f0e64c15876611db301f4e
suntao <358777330@qq.com>
2021-10-01 15:35:00 +0800

    init

---
+++ app1/views.py
@@ -0,0 +1,10 @@
+import time
+
+from django.shortcuts import render
+from django.http.response import HttpResponse
+
+# Create your views here.
+
+def demo(requests):
+    time.sleep(10)
+    return HttpResponse("test")
